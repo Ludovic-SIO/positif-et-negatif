5e214e9ef039f9e43fbabedef17fd6ac5b11ad37
ludovic <ludovic@DESKTOP-MQSNRHN>
2024-11-21 20:54:55 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    
+    internal class Program
+    {
+        static int uyfgchsklufevyg(int a)
+        {
+
+            if (a <= 0) return -a;
+
+            else return a;
+        }
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("met un nombre ");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("le nombre est {0}",a);
+            Console.WriteLine("le nombre est {0}", uyfgchsklufevyg(a));
+
+        }
+    }
+}
